6407d5c7c45bd7ae280efe0a4b0f5ec292a54a3c
Ulisses <ulissessuporteti01@gmail.com>
2022-12-06 19:00:54 -0300

    criando o formulário da página de inventário

--- src/frontend/ByteShop/src/App.tsx
+++ src/frontend/ByteShop/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import React from "react";
+import { Alert } from "react-bootstrap";
+import Inventario from "./pages/Inventario";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    // <Alert key="primary" variant="primary" className="w-25">
+    //   This is a primary alert—check it out!
+    // </Alert>
+    <Inventario />
+  );
 }
 
 export default App;
