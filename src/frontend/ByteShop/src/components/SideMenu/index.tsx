e02afc07b0037b5ce188d97d13e19af056180b11
Ulisses <ulissessuporteti01@gmail.com>
2022-12-08 15:21:47 -0300

    criando o component Sidemenu

---
+++ src/frontend/ByteShop/src/components/SideMenu/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+  Offcanvas,
+  OffcanvasBody,
+  Row,
+  TabContainer,
+  TabContent,
+  TabPane,
+} from "react-bootstrap";
+import mnuHamburger from "./assets/icons/menu-hamburger.svg";
+import returnIcon from "./assets/icons/turn-left.svg";
+
+const SideMenu: React.FC = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="" title="Abrir menu" onClick={handleShow}>
+        {<img src={mnuHamburger} alt="ícone menu hamburger" />}
+      </Button>
+      <Offcanvas show={show} onHide={handleClose} className="offcanvas">
+        <OffcanvasBody className="offcanvas-body">
+          <TabContainer>
+            <Row>
+              <Col sm={3} className="m-3">
+                <Nav variant="pills">
+                  <NavItem>
+                    <NavLink eventKey="produtos">Produtos</NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+              <Col sm={3} className="m-3">
+                <TabContent>
+                  <TabPane eventKey="produtos">
+                    <Nav variant="pills">
+                      <NavItem>
+                        <NavLink eventKey="Cadastrar produtos">
+                          Cadastrar Produtos
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink eventKey="Iventário">Inventário</NavLink>
+                      </NavItem>
+                    </Nav>
+                  </TabPane>
+                </TabContent>
+              </Col>
+            </Row>
+          </TabContainer>
+        </OffcanvasBody>
+        <Button variant="" onClick={handleClose}>
+          {<img src={returnIcon} alt="ícone return" />}
+        </Button>
+      </Offcanvas>
+    </>
+  );
+};
+
+export default SideMenu;
