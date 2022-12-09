795f0fbc717e5fa33a20fcfcc1ac5e49e52ed2c8
Ulisses <ulissessuporteti01@gmail.com>
2022-12-08 23:00:11 -0300

    adicionando duas rotas provisórias em APP.tsx

--- src/frontend/ByteShop/src/App.tsx
+++ src/frontend/ByteShop/src/App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Paginacao from "./components/Paginacao";
+import SideMenu from "./components/SideMenu";
+import CadastroProduto from "./pages/CadastroProduto/CadastroProduto";
 import Inventario from "./pages/Inventario";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    // <Alert key="primary" variant="primary" className="w-25">
-    //   This is a primary alert—check it out!
-    // </Alert>
-    <Inventario />
+    <Container fluid className="vw-100 vh-100 overflow-auto">
+      <Router>
+        <SideMenu />
+        <Routes>
+          <Route path={"/"} element={<Inventario />} />
+          <Route path={"/cadastro-de-produtos"} element={<CadastroProduto />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
 export default App;
+function useState(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.");
+}
