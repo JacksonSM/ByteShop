cee69989d513c886e4cb6c47a29fe8818ae0facf
JacksonSM <jacksons.m2018@gmail.com>
2022-12-16 09:31:49 -0300

    test: feito testes de unitade para validação dos campos em atualizar produtos.

--- tests/Utilities/Commands/ProductCommandBuilder.cs
+++ tests/Utilities/Commands/ProductCommandBuilder.cs
@@ -29,4 +29,24 @@ public class ProductCommandBuilder
             .RuleFor(c => c.MainImageBase64, imagesFaker.Generate())
             .RuleFor(c => c.SecondaryImagesBase64, imagesFaker.Generate(numberSecondaryImages).ToArray());
     }
+
+    public static UpdateProductCommand UpdateProductCommandBuild(int numberSecondaryImages = 3)
+    {
+        return new Faker<UpdateProductCommand>()
+            .RuleFor(c => c.Name, f => f.Commerce.ProductName())
+            .RuleFor(c => c.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(c => c.SKU, f => f.Commerce.Ean13())
+            .RuleFor(c => c.Brand, f => f.Lorem.Word())
+            .RuleFor(c => c.Price, f => f.Finance.Amount(min: 1))
+            .RuleFor(c => c.CostPrice, f => f.Finance.Amount(min: 1))
+            .RuleFor(c => c.Stock, f => f.Random.Number(1000))
+            .RuleFor(c => c.Warranty, f => f.Random.Number(200))
+            .RuleFor(c => c.Weight, f => f.Random.Float(1))
+            .RuleFor(c => c.Height, f => f.Random.Float(1))
+            .RuleFor(c => c.Width, f => f.Random.Float(1))
+            .RuleFor(c => c.Length, f => f.Random.Float(1))
+            .RuleFor(c => c.CategoryId, f => f.Random.Number(500))
+            .RuleFor(c => c.MainImageUrl, f => f.Internet.Url())
+            .RuleFor(c => c.SecondaryImageUrl, f => f.Make(numberSecondaryImages, () => f.Internet.Url()).ToArray());
+    }
 }
---
+++ tests/Validators.Test/Products/UpdateProductValidationTest.cs
@@ -0,0 +1,153 @@
+﻿using ByteShop.Application.UseCases.Validations.Product;
+using ByteShop.Exceptions;
+using FluentAssertions;
+using Utilities.Commands;
+using Xunit;
+
+namespace Validators.Test.Products;
+public class UpdateProductValidationTest
+{
+    [Fact]
+    public void Sucesso()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidarErroNomeVazio()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Name = string.Empty;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_NAME_EMPTY));
+    }
+    [Fact]
+    public void ValidarErroNomeMaiorQue60Caracteres()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Name = string.Join("", Enumerable.Repeat("x", 66));
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_NAME_MAXIMUMLENGTH));
+    }
+
+    [Fact]
+    public void ValidarErroMarcaVazio()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Brand = string.Empty;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_BRAND_EMPTY));
+    }
+    [Fact]
+    public void ValidarErroMarcaMaiorQue30Caracteres()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Brand = string.Join("", Enumerable.Repeat("x", 34));
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_BRAND_MAXIMUMLENGTH));
+    }
+
+    [Fact]
+    public void ValidarErroDescricaoVazio()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Description = string.Empty;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_DESCRIPTION_EMPTY));
+    }
+    [Fact]
+    public void ValidarErroSKUVazio()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.SKU = string.Empty;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_SKU_EMPTY));
+    }
+    [Fact]
+    public void ValidarErroPesoMenorOuIgualZero()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Weight = -2f;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_WEIGHT_LESS_OR_EQUAL_TO_ZERO));
+    }
+    [Fact]
+    public void ValidarErroAlturaMenorOuIgualZero()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Height = -2f;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_HEIGHT_LESS_OR_EQUAL_TO_ZERO));
+    }
+    [Fact]
+    public void ValidarErroLarguraMenorOuIgualZero()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Width = 0;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_WIDTH_LESS_OR_EQUAL_TO_ZERO));
+    }
+    [Fact]
+    public void ValidarErroComprimentoMenorOuIgualZero()
+    {
+        var validator = new UpdateProductValidation();
+        var command = ProductCommandBuilder.UpdateProductCommandBuild();
+        command.Length = -2.0f;
+
+        var result = validator.Validate(command);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And
+            .Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.PRODUCT_LENGTH_LESS_OR_EQUAL_TO_ZERO));
+    }
+}
