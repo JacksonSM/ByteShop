0f7d58f3768ddb185e09574d654356ad4dbb89f2
JacksonSM <jacksons.m2018@gmail.com>
2022-12-16 09:52:06 -0300

    test: feito testes de unitade para validação dos campos em adicionar e atualizar categoria.

---
+++ tests/Utilities/Commands/CategoryCommandBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using ByteShop.Application.UseCases.Commands.Category;
+
+namespace Utilities.Commands;
+public class CategoryCommandBuilder
+{
+    public static AddCategoryCommand AddCategoryCommandBuild()
+    {
+        return new Faker<AddCategoryCommand>()
+            .RuleFor(c => c.Name, f => f.Commerce.Department())
+            .RuleFor(c => c.ParentCategoryId, f => f.Random.Number(500));
+    }
+    public static UpdateCategoryCommand UpdateCategoryCommandBuild()
+    {
+        return new Faker<UpdateCategoryCommand>()
+            .RuleFor(c => c.Name, f => f.Commerce.Department())
+            .RuleFor(c => c.ParentCategoryId, f => f.Random.Number(500));
+    }
+}
