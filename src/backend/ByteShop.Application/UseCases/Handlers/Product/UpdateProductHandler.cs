f07f6c8c6cb8021aef7589539320c08ffbfaa657
JacksonSM <jacksons.m2018@gmail.com>
2022-12-21 19:45:35 -0300

    refactor: handler de atualizar produto.

--- src/backend/ByteShop.Application/UseCases/Handlers/Product/UpdateProductHandler.cs
+++ src/backend/ByteShop.Application/UseCases/Handlers/Product/UpdateProductHandler.cs
@@ -1,13 +1,12 @@
 ﻿using AutoMapper;
 using ByteShop.Application.DTOs;
+using ByteShop.Application.Services;
 using ByteShop.Application.UseCases.Commands.Product;
 using ByteShop.Application.UseCases.Results;
 using ByteShop.Application.UseCases.Validations.Product;
 using ByteShop.Domain.Interfaces.Repositories;
-using ByteShop.Exceptions.Exceptions;
 using ByteShop.Exceptions;
-using ByteShop.Application.Services;
-using static System.Net.Mime.MediaTypeNames;
+using ByteShop.Exceptions.Exceptions;
 
 namespace ByteShop.Application.UseCases.Handlers.Product;
 public class UpdateProductHandler : IHandler<UpdateProductCommand, ProductDTO>
@@ -57,6 +56,33 @@ public class UpdateProductHandler : IHandler<UpdateProductCommand, ProductDTO>
                 categoryId: command.CategoryId
             );
 
+        if (command.SetMainImageBase64 is not null)
+            await _imageService.DeleteImageAsync(product.MainImageUrl);
+
+        if (command.RemoveSecondaryImageUrl?.Length > 0)
+        {
+            await _imageService.DeleteImageAsync(command.RemoveSecondaryImageUrl);
+        }
+
+        if (command.SetMainImageBase64 != null)
+        {
+            var mainImageUrl = await _imageService.UploadBase64ImageAsync(
+                command.SetMainImageBase64.Base64,
+                command.SetMainImageBase64.Extension);
+
+            product.SetMainImage(mainImageUrl);
+        }
+
+        if (command.AddSecondaryImageBase64?.Length > 0)
+        {
+            foreach (var imageBase64 in command.AddSecondaryImageBase64)
+            {
+                var url = await _imageService.UploadBase64ImageAsync(imageBase64.Base64,
+                    imageBase64.Extension);
+                product.AddSecondaryImage(url);
+            }
+        }
+
         _productRepo.Update(product);
         await _uow.CommitAsync();
 
@@ -67,7 +93,7 @@ public class UpdateProductHandler : IHandler<UpdateProductCommand, ProductDTO>
     private async Task ValidateAsync(UpdateProductCommand command,
         Domain.Entities.Product product)
     {
-        var validator = new UpdateProductValidation();
+        var validator = new UpdateProductValidation(product);
         var validationResult = validator.Validate(command);
 
         var IsThereCategory = await _categoryRepo.ExistsById(command.CategoryId);
@@ -75,26 +101,14 @@ public class UpdateProductHandler : IHandler<UpdateProductCommand, ProductDTO>
                 .Add(new FluentValidation.Results.ValidationFailure(string.Empty,
                 ResourceErrorMessages.CATEGORY_DOES_NOT_EXIST));
 
-        if (command.AreThereImagesToAdd())
-        {
-            var finalAmount = FinalAmountOfImages(command, product);
-            if (finalAmount > 5) validationResult.Errors
-                .Add(new FluentValidation.Results.ValidationFailure(string.Empty,
-                ResourceErrorMessages.MAXIMUM_AMOUNT_OF_IMAGES));
-        }
 
         if (!validationResult.IsValid)
         {
             var errorMessages = validationResult.Errors.Select(c => c.ErrorMessage).ToList();
             throw new ValidationErrorsException(errorMessages);
         }
-    }
 
-    private int FinalAmountOfImages(UpdateProductCommand command,
-        Domain.Entities.Product product)
-    {
-        // refatorar o nome
-        var result = product.GetImagesTotal() - command.GetTotalImagesToRemove();
-        return result + command.GetTotalImagesToAdd();
     }
 }
+
+
