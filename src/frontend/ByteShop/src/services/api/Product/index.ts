5b6c38efdb6ff084e9868bd4588696d69c58ce89
Ulisses <ulissessuporteti01@gmail.com>
2022-12-24 11:07:04 -0300

    fix(API): corrigindo o try/catch de Product.Post()

--- src/frontend/ByteShop/src/services/api/Product/index.ts
+++ src/frontend/ByteShop/src/services/api/Product/index.ts
@@ -1,4 +1,4 @@
-import { AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import { api } from "../../axios-config";
 import { IProductGet, IProductPost } from "./types";
 
@@ -40,30 +40,33 @@ async function getById(id: number): Promise<IProductGet[] | Error> {
 }
 
 async function post({ ...attribute }: IProductPost): Promise<any | Error> {
+  const values: IProductPost | any = {
+    name: attribute.name,
+    description: attribute.description,
+    sku: attribute.sku,
+    price: attribute.price,
+    costPrice: attribute.costPrice,
+    stock: attribute.stock,
+    warranty: attribute.warranty,
+    brand: attribute.brand,
+    weight: attribute.weight,
+    height: attribute.weight,
+    length: attribute.length,
+    width: attribute.width,
+    categoryId: attribute.categoryId,
+    mainImageBase64: attribute.mainImageBase64,
+    secondaryImagesBase64: attribute.secondaryImagesBase64,
+  };
   try {
-    const values: IProductPost | any = {
-      name: attribute.name,
-      description: attribute.description,
-      sku: attribute.sku,
-      price: attribute.price,
-      costPrice: attribute.costPrice,
-      stock: attribute.stock,
-      warranty: attribute.warranty,
-      brand: attribute.brand,
-      weight: attribute.weight,
-      height: attribute.weight,
-      length: attribute.length,
-      width: attribute.width,
-      categoryId: attribute.categoryId,
-      mainImageBase64: attribute.mainImageBase64,
-      secondaryImagesBase64: attribute.secondaryImagesBase64,
-    };
-
-    console.log(values);
-
-    return api.post("product", values).then((response) => response.data.data);
+    const { data } = await api.post("product", values);
+    if (data) {
+      return { data };
+    }
+    return new Error("Erro ao cadastrar o produto");
   } catch (error) {
-    throw new Error((error as { message: string }).message);
+    return new Error(
+      (error as { message: string }).message || "Erro ao cadastrar o produto"
+    );
   }
 }
 
