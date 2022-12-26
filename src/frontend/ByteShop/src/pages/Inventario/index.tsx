b2bf48c66a4bd8108e9ebed5b5fd37ff516b4fab
Ulisses <ulissessuporteti01@gmail.com>
2022-12-26 14:41:04 -0300

    style: update estilo do inventário

--- src/frontend/ByteShop/src/pages/Inventario/index.tsx
+++ src/frontend/ByteShop/src/pages/Inventario/index.tsx
@@ -22,64 +22,66 @@ const Inventario: React.FC = () => {
         </BreadcrumbItem>
       </Breadcrumb>
       <Form
-        className=" d-flex flex-wrap m-auto p-3 form-inventario"
+        className=" d-flex w-100 justify-content-center shadow-sm  flex-wrap m-auto p-3 form-inventario"
         style={{ width: "800px" }}
       >
-        <FormGroup className="m-1">
+        <FormGroup className="m-1" style={{width: "fit-content"}}>
           <FormLabel htmlFor="sku" className="m-1">
             Código
           </FormLabel>
           <FormControl
             type="text"
             id="sku"
-            style={{ width: "15.625rem" }}
+            style={{ width: "18.625rem" }}
             placeholder="ssd480-king"
           ></FormControl>
         </FormGroup>
-        <FormGroup className="m-1">
+        <FormGroup className="m-1" style={{width: "fit-content"}} >
           <FormLabel htmlFor="nome" className="m-1">
             Nome do Produto
           </FormLabel>
           <FormControl
             type="text"
             id="nome"
-            style={{ width: "31.25rem" }}
+            style={{ minWidth: "18.625rem" }}
             placeholder="SSD 480GB Kingston"
           ></FormControl>
         </FormGroup>
-        <FormGroup className="m-1">
+        <FormGroup className="m-1"  style={{width: "fit-content"}} >
           <FormLabel htmlFor="marca" className="m-1">
             Marca
           </FormLabel>
           <FormControl
             type="text"
             id="marca"
-            style={{ width: "18.75rem" }}
+            style={{ width: "19.625rem" }}
             placeholder="Kingston"
           ></FormControl>
         </FormGroup>
-        <FormGroup className="m-1">
+        <FormGroup className="m-1"  style={{width: "fit-content"}}>
           <FormLabel htmlFor="categoria" className="m-1">
             Categoria
           </FormLabel>
           <FormControl
             type="text"
             id="categoria"
-            style={{ width: "18.75rem" }}
+            style={{ maxWidth: "19.625rem" }}
             placeholder="SSD"
           ></FormControl>
         </FormGroup>
         <FormGroup
-          className="d-flex justify-content-between align-self-end my-1 ms-1"
+          className="align-self-end my-1 ms-1 w-auto"
           style={{ width: "150px", height: "37px" }}
         >
           <Button
+            className="me-2"
             variant="outline-primary"
             style={{ width: "69px", height: "37px" }}
-          >
+            >
             Filtar
           </Button>
           <Button
+            className="me-auto"
             variant="outline-danger"
             style={{ width: "69px", height: "37px" }}
           >
