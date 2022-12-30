d09831076354dd3d54481b92911a099c5da1395a
JacksonSM <jacksons.m2018@gmail.com>
2022-12-30 16:56:01 -0300

    fix: removendo a referencia obrigatoria de categoria pai.

---
+++ src/backend/ByteShop.Infrastructure/Migrations/20221230192813_REMOVING_MANDATORY_CATEGORY_REFERENCE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ByteShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class REMOVINGMANDATORYCATEGORYREFERENCE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
