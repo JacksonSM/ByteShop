3e59344dc5a3bcbfebdf13af4533a384c037a1ed
Ulisses <ulissessuporteti01@gmail.com>
2023-01-06 15:02:52 -0300

    refactor: removendo a unterface IcategoryPutAndpost ... ajustando os parametros das funções

--- src/frontend/ByteShop/src/services/api/Category/types/index.ts
+++ src/frontend/ByteShop/src/services/api/Category/types/index.ts
@@ -1,11 +1,4 @@
 export interface Icategory {
-  id: number;
-  name: string;
-  categoryLevel?: number;
-  parentCategoryId: number;
-}
-
-export interface IcategoryPutAndpost {
   id?: number;
   name: string;
   categoryLevel?: number;
