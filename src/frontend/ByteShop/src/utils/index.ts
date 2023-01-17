04adf8d3f42312921c07defdd5aa32d79f621706
Ulisses <ulissessuporteti01@gmail.com>
2023-01-17 20:34:42 -0300

    feat: adicionando algumas Regex para as URL das imagens

--- src/frontend/ByteShop/src/utils/index.ts
+++ src/frontend/ByteShop/src/utils/index.ts
@@ -1,6 +1,20 @@
-const {...print} = console;
+const { ...print } = console;
 const replacingComma = (value: string) => value.replaceAll(",", ".");
 const replacingDot = (value: string) => value.replaceAll(".", ",");
 
+const urlRegex =
+  /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+const base64Regex =
+  /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+const base64ReplacePatternRegex = /data:.*base64,/;
+const base64ExtensionReplaceReg= /^\w*[/]/;
 
-export  {print, replacingComma, replacingDot};
+export {
+  print,
+  replacingComma,
+  replacingDot,
+  urlRegex,
+  base64Regex,
+  base64ReplacePatternRegex,
+  base64ExtensionReplaceReg,
+};
